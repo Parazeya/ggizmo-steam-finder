c4c2c4e76b29f3c988889e8d985f128101ed6a49
AlexZ <zezarUS@bk.ru>
2021-10-04 22:18:27 +0300

    added icons & args

---
+++ README.md
@@ -0,0 +1,18 @@
+# steam-config-finder
+Automatic search Gizmo Server on the computer for Node.js (NOT OFFICIAL) 
+
+![GCGApp](https://ggbook.ru/AdminPanel/public/images/favicon-32x32.png)
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/keza3d/ggizmo-finder)
+
+## Installation
+Download file and start file `steam-config-finder.exe` 
+
+## Arguments
+`--filename`
+Custom output filename: --filename=example.json
+
+`--foldername`
+Custom folder: --foldername="C:\path\to"
+
+## License
+Steam-config-finder is released under the [MIT License](https://github.com/KeZA3D/ggizmo-api/blob/main/LICENSE)
--- index.js
+++ index.js
@@ -6,34 +6,80 @@ const { Registry } = require('rage-edit')
     , parser = require('steam-acf2json').decode
     , express = (require('express')()).listen();
 
-(async () => {
+
+; (async () => {
     try {
         const REGISTRY_PATH = "HKEY_CURRENT_USER\\Software\\Valve\\Steam"
         var SteamPath = await Registry.get(REGISTRY_PATH, 'SteamPath'),
-            SteamAppsDirectory, SteamAppFolders, SteamAppVDF, i, e, manifest, outputData = []
+            SteamAppsDirectory, SteamAppFolders, SteamAppVDF, i, e, sourceIcon, source600x900, iconFolder = folder() + "\\icons\\", manifest, outputData = [];
+        if (!fs.existsSync(folder() + "\\icons")) {
+            fs.mkdirSync(folder() + "\\icons")
+        }
         if (SteamPath === undefined) throw new Error("Cant find Steam registry: " + REGISTRY_PATH)
         SteamAppFolders = parser(fs.readFileSync(SteamPath + "\\config\\libraryfolders.vdf", 'utf8'))
         if (SteamAppFolders === undefined) throw new Error("Cant read file:", "LibraryFolders.vdf")
         SteamAppVDF = Object.values(SteamAppFolders.libraryfolders).filter(t => t.path !== undefined)
-        if (SteamAppFolders === undefined) throw new Error("Cant read file:", "LibraryFolders.vdf")
-        for (i in SteamAppVDF) {
-            SteamAppsDirectory = fs.readdirSync(SteamAppVDF[i].path.replace(/\\\\/g, "/") + "/steamapps").filter(f => f.split(".").pop() === "acf")
-            // console.log(SteamAppVDF[i].path.replace(/\\\\/g, "/") + "/steamapps", SteamAppsDirectory[0] === undefined)
-            if (SteamAppsDirectory[0] === undefined) continue;
-            for (e in SteamAppsDirectory) {
-                manifest = parser(fs.readFileSync(SteamAppVDF[i].path.replace(/\\\\/g, "/") + "/steamapps/" + SteamAppsDirectory[e], 'utf8'))
+        if (SteamAppVDF === undefined || SteamAppVDF[0] === undefined) {
+            SteamAppsDirectory = fs.readdirSync(SteamPath.replace(/\\\\/g, "/") + "/steamapps").filter(f => f.split(".").pop() === "acf")
+            if (SteamAppsDirectory[0] === undefined) throw new Error("0 manifest finded!")
+            for (i in SteamAppsDirectory) {
+                manifest = parser(fs.readFileSync(SteamPath.replace(/\\\\/g, "/") + "/steamapps/" + SteamAppsDirectory[i], 'utf8'))
+                sourceIcon = SteamPath + "\\appcache\\librarycache\\" + manifest.AppState.appid + "_icon.jpg";
+                source600x900 = SteamPath + "\\appcache\\librarycache\\" + manifest.AppState.appid + "_library_600x900.jpg"
+                if (!fs.existsSync(iconFolder + manifest.AppState.appid + "_icon.jpg") && fs.existsSync(sourceIcon)) fs.copyFileSync(sourceIcon, iconFolder + manifest.AppState.appid + "_icon.jpg")
+                if (!fs.existsSync(iconFolder + manifest.AppState.appid + "_library_600x900.jpg") && fs.existsSync(source600x900)) fs.copyFileSync(source600x900, iconFolder + manifest.AppState.appid + "_library_600x900.jpg")
                 outputData.push({
                     Caption: manifest.AppState.name,
                     ExecutablePath: manifest.AppState.LauncherPath.replace(/\\\\/g, "\\"),
                     Arguments: "steam://rungameid/" + manifest.AppState.appid,
-                    WorkingDirectory: manifest.AppState.LauncherPath.replace(/\\\\/g, "\\").replace("\\steam.exe","")
+                    WorkingDirectory: manifest.AppState.LauncherPath.replace(/\\\\/g, "\\").replace("\\steam.exe", "")
                 })
             }
+        } else {
+            for (i in SteamAppVDF) {
+                SteamAppsDirectory = fs.readdirSync(SteamAppVDF[i].path.replace(/\\\\/g, "/") + "/steamapps").filter(f => f.split(".").pop() === "acf")
+                // console.log(SteamAppVDF[i].path.replace(/\\\\/g, "/") + "/steamapps", SteamAppsDirectory[0] === undefined)
+                if (SteamAppsDirectory[0] === undefined) continue;
+
+                for (e in SteamAppsDirectory) {
+                    manifest = parser(fs.readFileSync(SteamAppVDF[i].path.replace(/\\\\/g, "/") + "/steamapps/" + SteamAppsDirectory[e], 'utf8'))
+                    if (manifest.AppState.appid == 228980) continue;
+                    sourceIcon = SteamPath + "\\appcache\\librarycache\\" + manifest.AppState.appid + "_icon.jpg";
+                    source600x900 = SteamPath + "\\appcache\\librarycache\\" + manifest.AppState.appid + "_library_600x900.jpg"
+                    if (!fs.existsSync(iconFolder + manifest.AppState.appid + "_icon.jpg") && fs.existsSync(sourceIcon)) fs.copyFileSync(sourceIcon, iconFolder + manifest.AppState.appid + "_icon.jpg")
+                    if (!fs.existsSync(iconFolder + manifest.AppState.appid + "_library_600x900.jpg") && fs.existsSync(source600x900)) fs.copyFileSync(source600x900, iconFolder + manifest.AppState.appid + "_library_600x900.jpg")
+                    outputData.push({
+                        Caption: manifest.AppState.name,
+                        ExecutablePath: manifest.AppState.LauncherPath.replace(/\\\\/g, "\\"),
+                        Arguments: "steam://rungameid/" + manifest.AppState.appid,
+                        WorkingDirectory: manifest.AppState.LauncherPath.replace(/\\\\/g, "\\").replace("\\steam.exe", "")
+                    })
+                }
+            }
         }
-        console.log(process.cwd())
-        fs.writeFileSync(process.cwd() + "\\apps.json",JSON.stringify(outputData))
-        console.log("DONE")
+        fs.writeFileSync(`${folder()}\\${filename()}`, JSON.stringify(outputData))
+        console.log("Done, close this application. Created file: " + filename())
     } catch (e) {
         console.log(e.message)
     }
 })()
+
+function filename() {
+    var Arguments = process.argv,
+        filename = Arguments.find(e => e.search(/--filename=/) != -1)
+    if (filename === undefined) filename = "apps.json"
+    else {
+        filename = filename.split("=").pop()
+    }
+    return filename
+}
+
+function folder() {
+    var Arguments = process.argv,
+        foldername = Arguments.find(e => e.search(/--foldername=/) != -1)
+    if (foldername === undefined) foldername = process.cwd()
+    else {
+        foldername = foldername.split("=").pop()
+    }
+    return foldername
+}
